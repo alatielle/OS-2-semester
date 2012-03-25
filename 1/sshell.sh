f9f802cdf510477a1f994c32be5781c20d36f015
Elena Rashkovan <mi.usu@ya.ru>
2012-03-26 05:54:03 +0600

    Error parsing +

--- 1/sshell.sh
+++ 1/sshell.sh
@@ -3,9 +3,14 @@
 set -e
 
 host='192.168.0.1'
-rate=10
-size=$(xwininfo -root | awk '/.*-geometry.*/ {match ($2,"[0-9]+x[0-9]+"); print substr($2,RSTART,RLENGTH)}')
-screen=':0.0'
+rate=$1
+if [ $4 -eq 0 -o $5 -eq 0 ]
+then
+	size=$(xwininfo -root | awk '/.*-geometry.*/ {match ($2,"[0-9]+x[0-9]+"); print substr($2,RSTART,RLENGTH)}')
+else
+	size=$4'x'$5
+fi
+screen=':'$2'.'$3
 
 ssh $host << EOP
 ffmpeg -f x11grab -r $rate -s $size -i $screen -f avi pipe:1
--- 1/start.sh
+++ 1/start.sh
@@ -2,4 +2,39 @@
 
 set -e
 
-./sshell.sh | ffplay -f avi -
+FR=10
+D=0
+S=0
+X=0
+Y=0
+args=("$@")
+i=0
+while [ $i -lt $# ]
+do
+	if [[ ${args[$i]} =~ ^[-][Ff][Rr]$ && ${args[i+1]} =~ ^[0-9]+ ]]
+	then
+		FR=${args[$i+1]}
+		let "i+=2"
+	elif [[ ${args[$i]} =~ ^[-][Dd]$ && ${args[i+1]} =~ ^[0-9]+$ ]]
+	then
+		D=${args[i+1]}
+		let "i+=2"
+	elif [[ ${args[$i]} =~ ^[-][sS]$ && ${args[i+1]} =~ ^[0-9]+$ ]]
+	then
+		S=${args[i+1]}
+		let "i+=2"
+	elif [[ ${args[$i]} =~ ^[-][xX]$ && ${args[i+1]} =~ ^[0-9]+$ ]]
+	then
+		X=${args[i+1]}
+		let "i+=2"
+	elif [[ ${args[$i]} =~ ^[-][yY]$ && ${args[i+1]} =~ ^[0-9]+$ ]]
+	then
+		Y=${args[i+1]}
+		let "i+=2"
+	else 
+		echo "Incorrect command line args"
+		exit
+	fi
+done
+
+./sshell.sh $FR $D $S $X $Y | ffplay -f avi -
