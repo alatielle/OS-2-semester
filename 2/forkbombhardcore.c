70b9eb98f62ca9ef99aa214be75faa19699cf925
Elena Rashkovan <mi.usu@ya.ru>
2012-04-20 12:30:56 +0600

    improved

--- 2/forkbombhardcore.c
+++ 2/forkbombhardcore.c
@@ -2,20 +2,25 @@
 #include <stdio.h>
 
 int main() {
-	if(!fork()) {
-		if(setpgid(0, 0) == -1) {
-			perror("Can't set PGID\n");
-			return 1;
-		}
-		pid_t pgid = getpgid(0);
-		if(pgid == -1) {
-			perror("Can't get PGID\n");
-			return 1;
-		}
-		if(printf("%d", pgid) < 0) {
-			return 1;
-		}
-		while(!fork());
+	setbuf(stdout, NULL);
+	if(setpgid(0, 0) == -1) {
+		perror("Can't set PGID");
+		return 1;
+	}
+	pid_t pgid = getpgid(0);
+	if(pgid == -1) {
+		perror("Can't get PGID");
+		return 1;
+	}
+	if(printf("%d\n", pgid) < 0) {
+		return 1;
+	}
+	if(fork() == -1) {
+		perror("Can't fork");
+		return 1;
+	}
+	while(1) {
+		fork();
 	}
 	return 0;
 }
