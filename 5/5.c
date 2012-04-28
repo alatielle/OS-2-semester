2fdee55f86270f052a5abc74bd70cc3604dbf183
Alexander Braun <SCraftworld@yandex.ru>
2012-04-28 18:22:14 +0600

    Task 5 finished: error handling, aio_suspend, minor changes

--- 5/5.c
+++ 5/5.c
@@ -3,24 +3,50 @@
 #include <stdlib.h>
 #include <string.h>
 #include <aio.h>
+#include <fcntl.h>
 #include <errno.h>
+#include <time.h>
+
+#define buf_size 100
+
+struct aiocb* cba;
+struct aiocb** cblist;
+char** ba;
+char* stop;
+int maxInd;
+
+void freeAll()
+{
+	int i;
+	free(cba);
+	free(cblist);
+	free(stop);
+	if (ba!=NULL)
+		for (i=0; i<maxInd; ++i)
+			free(ba[i]);
+	free(ba);
+}
 
 int main(int argc, char * argv[])
 {
 	int i;
 	int s;
-	struct aiocb* cba;
-	char** ba;
-	char* stop;
 	char next=1;
 	if (argc<2)
 	{
 		printf("Too few params\n");
 		exit(1);
 	}
+	maxInd=argc-1;
 	cba=(struct aiocb*)calloc(argc-1, sizeof(struct aiocb));
+	cblist=(struct aiocb**)calloc(argc-1, sizeof(struct aiocb*));
 	ba=(char**)calloc(argc-1, sizeof(char*));
 	stop=(char*)calloc(argc-1, sizeof(char));
+	if (cba==NULL || cblist==NULL || ba==NULL || stop==NULL)
+	{
+		printf("Memory allocation error\n");
+		freeAll();
+	}
 	for (i=0; i<argc-1; ++i)
 	{
 		stop[i]=0;
@@ -30,49 +56,70 @@ int main(int argc, char * argv[])
 		if (fd<0)
 		{
 			perror("Open failure");
+			freeAll();
 			exit(2);
 		}
 		cba[i].aio_fildes=fd;
 		cba[i].aio_offset=0;
-		ba[i]=(char*)calloc(101,sizeof(char));
+		ba[i]=(char*)calloc(buf_size+1,sizeof(char));
 		cba[i].aio_buf=ba[i];
-		cba[i].aio_nbytes=100;
+		cba[i].aio_nbytes=buf_size;
 		cba[i].aio_reqprio=0;
+		cblist[i]=&cba[i];
 		s=aio_read(&cba[i]);
 		if (s<0)
 		{
 			perror("AIO read failure");
+			freeAll();
 			exit(2);
 		}
 	}
 	while (next)
 	{
 		next=0;
+		while (aio_suspend(cblist,argc-1,0)<0)
+			if (errno==EINTR) continue;
+				else break;
 		for (i=0; i<argc-1; ++i)
 		{
 			if (stop[i]) continue;
 			next=1;
-			while (aio_error(&cba[i])==EINPROGRESS)
-				sleep(1);
+			if (aio_error(&cba[i])==EINPROGRESS) continue;
+			
+			if (aio_error(&cba[i])!=0)
+			{
+				perror("AIO read error");
+				freeAll();
+				exit(2);
+			}
 			int actRead=aio_return(&cba[i]);
 			
 			if (actRead==0)
 			{
 				stop[i]=1;
-				printf("%d Nothing to read.\n",i);
-				close(cba[i].aio_fildes);
+				printf("Entry # %d: fd %d - nothing to read.\n",i ,cba[i].aio_fildes);
 			}
 			else
 			{
 				if (actRead<0)
 				{
 					perror("AIO return failure");
+					freeAll();
 					exit(2);
 				}
-				printf("%d %s\n\n",i,ba[i]);
-
+				printf("Entry # %d: fd %d - %d bytes read.\n---------------\n",i, cba[i].aio_fildes,actRead);
+				printf("%s\n\n",ba[i]);
+				cba[i].aio_offset+=actRead;
+				s=aio_read(&cba[i]);
+				if (s<0)
+				{
+					perror("AIO read failure");
+					freeAll();
+					exit(2);
+				}
 			}
 		}
 	}
+	freeAll();
 	return 0;
 }
